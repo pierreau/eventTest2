5255131f4f4f1a99ca97fec51364bc6ff6706e50
pierreau <pierreau@outlook.com>
2015-12-07 18:01:34 -0800

    Initial commit

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by pierrea on 12/7/15.
+ */
+public class helloWorld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello I am awesome!");
+    }
+}
